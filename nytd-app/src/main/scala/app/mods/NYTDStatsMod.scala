5e1bc3b33c40270487a847467f1dbe2193641557
Aleksandr-Grachev <alex.f.grach@gmail.com>
2024-09-17 16:25:44 +0300

    NYTDStatsMod added

---
+++ nytd-app/src/main/scala/app/mods/NYTDStatsMod.scala
@@ -0,0 +1,34 @@
+package app.mods
+
+import org.apache.spark.sql.SparkSession
+import org.apache.log4j.LogManager
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import java.time.ZoneId
+import java.time.ZoneOffset
+
+object NYTDStatsMod {
+
+  import app.datasets._
+  import app.models.config.AppConfig
+
+  private val log = LogManager.getLogger("NYTDStatsMod")
+
+  def run(appConfig: AppConfig)(implicit spark: SparkSession): Unit = {
+
+    log.debug("Running NYTDStatsMod")
+
+    val nytDataSet: NYTDataSets =
+      NYTDataSets(datasetDir = appConfig.files.datasetDir)(spark)
+
+    import nytDataSet._
+    println(s"Num partitions[${yellowTripDataDS.rdd.getNumPartitions}]")
+    yellowTripDataDS.explain()
+    yellowTripDataDS.printSchema()
+    //  log.warn(s"yellowTripDataDS.count[${yellowTripDataDS.count()}]")
+
+    yellowTripDataDS.show(100)
+
+  }
+
+}
